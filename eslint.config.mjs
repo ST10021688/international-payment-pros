d3c87dae6fb7b7edd1b34a3655e48e997886065b
Joe Russell <100833741+ST10172466@users.noreply.github.com>
2024-10-05 10:49:10 +0200

    ESLint Bug Fixes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,9 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
   {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -13,13 +12,17 @@ export default [
         ...globals.jest,
       },
       parserOptions: {
-        ecmaVersion: 2020, // Use a version that supports structuredClone
+        ecmaVersion: 2020, // Adjust based on your needs
+        sourceType: "module", // Allow the use of imports
       },
     },
     settings: {
       react: {
         version: "detect", // Automatically detect the React version
       },
+    },
+    rules: {
+      "constructor-super": "off", // Disable or adjust based on your requirements
     },
   },
   pluginJs.configs.recommended,
