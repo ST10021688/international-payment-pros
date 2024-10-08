3623cc510e85511892a6840440af3d416167d461
Joe Russell <100833741+ST10172466@users.noreply.github.com>
2024-10-08 15:25:28 +0200

    Merge.

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load environment variables
+/*require('dotenv').config(); // Load environment variables
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -39,6 +39,62 @@ app.use((err, req, res) => {
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// Graceful shutdown
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  console.log('MongoDB connection closed');
+  process.exit(0);
+});
+*/
+
+require('dotenv').config(); // Load environment variables
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const authRoutes = require('./routes/authRoutes'); 
+const connectToDatabase = require('./db/conn_db');
+const https = require('https'); // Include https module
+const fs = require('fs'); // File system to read SSL certificates
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// SSL Config: Load your SSL certificates
+const sslOptions = {
+  key: fs.readFileSync('./keys/server.key'), 
+  cert: fs.readFileSync('./keys/server.crt'),
+  ca: fs.readFileSync('./keys/myCA.pem') 
+};
+
+// Middleware
+app.use(helmet()); // Use helmet to secure the app by setting HTTP headers
+app.use(cors());
+app.use(express.json()); // Parse JSON request bodies
+
+connectToDatabase().catch(err => {
+  console.error('Failed to connect to the database:', err);
+  process.exit(1); // Exit the process with an error code
+});
+
+// Routes
+app.use('/api/auth', authRoutes); // Use authRoutes for authentication-related endpoints
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start the HTTPS server
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`Secure server is running on port ${PORT}`);
 });
 
 // Graceful shutdown
