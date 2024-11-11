5f60a53c348db723d89d48791d759d5eec2430e0
Joe Russell <100833741+ST10172466@users.noreply.github.com>
2024-11-11 17:12:49 +0200

    Switched back to port 5000.

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const fs = require('fs'); // File system to read SSL certificates
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 
 // SSL Config: Load your SSL certificates
 const sslOptions = {
